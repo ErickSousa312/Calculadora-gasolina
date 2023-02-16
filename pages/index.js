0abe59b9fffce6fb1c70c1feb11143098d591376
Erik sales <58636303+ErickSousa312@users.noreply.github.com>
2023-02-16 00:56:06 -0300

    criado o Css do projeto

--- pages/index.js
+++ pages/index.js
@@ -3,23 +3,54 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 import foto from '../public/img/foto-1.png'
+import { useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [dataForm, setDataForm] = useState({
+    name: '',
+    email: '',
+    subject: '',
+    content: ''
+  });
+  const onChangeInput = e => setDataForm({ ...dataForm, [e.target.name]: e.target.value });
+
   return (
     <>
       <div className={styles.thirteen}>
         <Image
           src='/img/combustivel.png'
           alt="13"
-          width={400}
-          height={310}
+          width={300}
+          height={300}
           priority
         />
         <h1 className={styles.font}>
-          Você sabe qual combustível compensa mais abastecer seu carro?
+          Você sabe qual combustível compensa mais abastecer seu carro?<br />
+
+          Utilize a calculadora abaixo:<br /><br />
         </h1>
+
+        <form>
+          <div className="fields">
+            <div className="field name">
+              <label className={styles.fontLabel}>Preço - litro etanol (R$):</label><br/>
+              <input className={styles.inputs} type="text" name="name"  onChange={onChangeInput} value={dataForm.name} />
+            </div>
+          </div>
+
+          <div className="fields">
+            <div className="field name">
+              <label className={styles.fontLabel}>Preço - litro gasolina (R$):</label><br/>
+              <input className={styles.inputs} type="email" name="email"  onChange={onChangeInput} value={dataForm.email} />
+            </div>
+          </div>
+          <div >
+            <button className={styles.buttonArea} type="submit">Calcular</button>
+          </div>
+        </form>
+
       </div>
 
     </>
